ea8fb9385b12fbba34fc04836faf5cf0810291f5
rubensnegrao <rubensnegraobezerra@gmail.com>
2025-01-12 11:11:30 -0300

    menu e banner

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {
+            colors: {
+                Marrom_White_01: "#F5E7DA",
+                Marrom_Medium_01: "#A97744",
+                Marrom_Black_01: "#292625",
+                Marrom_Black_02: "#3B2416",
+                Marrom_Medium_02: "#64221D",
+                Marrom_White_02: "#C7794A",
+                Marrom_White_03: "#CF9A5A",
+                Bright_Color: "#FDF5E3",
+                Marrom_white_00: "#E7E1DF"
+            }
+      },
+    },
+    plugins: [],
+  }
